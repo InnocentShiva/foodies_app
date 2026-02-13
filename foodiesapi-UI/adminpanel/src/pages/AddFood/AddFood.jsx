50ed166374964a844ec052041ae41d1e5fd15996
InnocentShiva <singh.akshat.solanki@gmail.com>
2026-02-14 03:20:53 +0530

    Add Form page UI

--- foodiesapi-UI/adminpanel/src/pages/AddFood/AddFood.jsx
+++ foodiesapi-UI/adminpanel/src/pages/AddFood/AddFood.jsx
@@ -1,8 +1,73 @@
 import React from 'react';
+import { assets } from '../../assets/assets';
+import { useState } from 'react';
 
 const AddFood = () => {
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: '',
+        description: '',
+        category: 'dosa',
+        price: ''
+    });
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setData(data => ({
+            ...data,
+            [name]: value
+        }));
+    }
+
+    
+
   return (
-    <div>AddFood</div>
+    <div className="mt-2">
+    <div className="row">
+        <div className="card col-md-8 col-lg-6">
+            <div className="card-body">    
+                <form className="contact-form">
+                    <h3 className="text-center mb-4">Add Food</h3>
+                    <div className="mb-3">
+                        <label htmlFor="image"  className="form-label">
+                            <img src={image ? URL.createObjectURL(image) : assets.upload} alt="upload Image" width={98}/>
+                        </label>
+                        <input type="file" className="form-control" id="image" name="image" required hidden onChange={(event) => setImage(event.target.files[0])}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" required name='name' onChange={onChangeHandler} value={data.name}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Description</label>
+                        <textarea className="form-control" id="description" rows="5" required name='description' onChange={onChangeHandler} value={data.description}></textarea>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="category" className="form-label">Category</label>
+                        <select name='category'  id='category'  className="form-control" onChange={onChangeHandler} value={data.category}>
+                            <option value="Burger">Dosa</option>
+                            <option value="Pizza">Pizza</option>
+                            <option value="Pasta">Pasta</option>
+                            <option value="Salad">Salad</option>
+                            <option value="Dessert">Dessert</option>
+                            <option value="Roll">Roll</option>
+                            <option value="Veg Biriyani">Veg Biriyani</option>
+                        </select>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="price" className="form-label">Price</label>
+                        <input type="number" name='price' id="price" className="form-control" onChange={onChangeHandler} value={data.price}/>
+                    </div>
+                    <div className="d-grid">
+                        <button type="submit" className="btn btn-primary btn-lg">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
